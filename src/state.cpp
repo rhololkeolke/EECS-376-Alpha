1e592188c9cca4ff0cb58a569a2062568fb12b2e
Devin Schwab <digidevin@gmail.com>
2012-02-27 10:08:57 -0500

    Added basic state unit tests for straights, arcs, and spins.  Arcs doesn't seem to be updating yPath correctly, but everything else appears to be working

--- src/state.cpp
+++ src/state.cpp
@@ -7,23 +7,31 @@
 
 #include "state.h"
 #include <math.h>
+#include <iostream>
+
+using namespace std;
 
 const double PI = 3.14159;
 
-State::State(double x0, double y0, double psi0)
+State::State()
 {
-	xPath = x0;
-	yPath = y0;
-	psiPath = psi0;
+	xPath = 0.0;
+	yPath = 0.0;
+	psiPath = 0.0;
+	segDistDone = 0.0;
 	currSeg = NULL;
+
+	//sub = n.subscribe("base_pose_ground_truth",1,poseCallback);
 }
 
-State::State(double x0, double y0, double psi0, eecs_376_alpha::PathSegment segment)
+State::State(eecs_376_alpha::PathSegment segment)
 {
-	xPath = x0;
-	yPath = y0;
-	psiPath = psi0;
-	currSeg = segment; // may make this pass by reference so that velocity profiler can update the ending positions and velocities via callbacks
+	xPath = segment.ref_point.x;
+	yPath = segment.ref_point.y;
+	psiPath = tf::getYaw(segment.init_tan_angle);
+	currSeg = &segment; // may make this pass by reference so that velocity profiler can update the ending positions and velocities via callbacks
+
+	//sub = n.subscribe("base_pose_ground_truth",1,poseCallback);
 }
 
 void State::updateState(double vCmd, double dt)
@@ -31,49 +39,83 @@ void State::updateState(double vCmd, double dt)
 	double dL = vCmd*dt;
 	double expSegDistDone = segDistDone + dL; // compute how much more the robot should complete based on previous segment distance done
 
-	if(currSeg.seg_type == 1) // straight line segment
+	if(currSeg == NULL) // if no path segment currently defined
+	{
+		return; // do nothing
+	}
+	if(currSeg->seg_type == 1) // straight line segment
 	{
 		// compute the theoretical line position
-		double psiDes = tf::getYaw(currSeg.init_tan_angle);
+		double psiPath = tf::getYaw(currSeg->init_tan_angle);
 		double rhoDes = 0; // this is zero because lines have no curvature
 		// line starts at xPath and yPath and is an an angle psiDes
-		double xDes = xPath+expSegDistDone*cos(psiDes);
-		double yDes = yPath+expSegDistDone*sin(psiDes);
-		// calculate the actual line position
+		xPath = currSeg->ref_point.x+expSegDistDone*cos(psiPath);
+		yPath = currSeg->ref_point.y+expSegDistDone*sin(psiPath);
 
+		// get the actual line position
+		//         May not actually need to do this here
 		// project actual onto desired
-
+		//         May not actually need to do this here
 		// calculate the segDistDone
+		segDistDone = expSegDistDone;
+		if(segDistDone >= currSeg->seg_length)
+		{
+			currSeg = NULL;
+		}
 	}
-	else if(currSeg.seg_type == 2) // arc segment
+	else if(currSeg->seg_type == 2) // arc segment
 	{
-		double rhoDes = currSeg.curvature;
+		cout << "running seg_type 2" << endl;
+		// calculate desired path
+		double rhoDes = currSeg->curvature;
 		double r = 1/fabs(rhoDes); // turn radius of inverse curvature
 		double arcAngleStart;
 
 		if(rhoDes >= 0)
-			arcAngleStart = tf::getYaw(currSeg.init_tan_angle)-PI/2;
+			arcAngleStart = tf::getYaw(currSeg->init_tan_angle)-PI/2;
 		else
-			arcAngleStart = tf::getYaw(currSeg.init_tan_angle)+PI/2;
+			arcAngleStart = tf::getYaw(currSeg->init_tan_angle)+PI/2;
 
 		double dAng = expSegDistDone*rhoDes;
 		double arcAng = arcAngleStart+dAng;
-		xDes = xPath + r*cos(arcAng);
-		yDes = yPath + r*sin(arcAng);
-		psiDes = tf::getYaw(currSeg.init_tan_angle)+dAng;
+		xPath = currSeg->ref_point.x + r*cos(arcAng);
+		cout << "y: " << currSeg->ref_point.y << endl;
+		cout << "r: " << r << endl;
+		cout << "arcAng: " << arcAng << endl;
+		yPath = currSeg->ref_point.y + r*sin(arcAng);
+		psiPath = tf::getYaw(currSeg->init_tan_angle)+dAng;
+
+		// calculate actual path
+		//         May not actually need to do this here
+		// compute projection of actual onto desired
+		//         May not actually need to do this here
+		// update segDistDone
+		segDistDone = expSegDistDone;
+		if(segDistDone >= currSeg->seg_length)
+			currSeg = NULL;
 	}
-	else if(currSeg.seg_type == 3)
+	else if(currSeg->seg_type == 3)
 	{
-		xDes = xPath;
-		yDes = yPath;
-		psiDes = tf::getYaw(currSeg.init_tan_angle) + currSeg.curvature()*expSegDistDone;
+		// calculate desired path
+		psiPath = tf::getYaw(currSeg->init_tan_angle) + currSeg->curvature*expSegDistDone;
+
+		// calculate actual path
+
+		// compute projection of actual onto desired
+
+		// update segDistDone
+		segDistDone = expSegDistDone;
+		if(segDistDone >= currSeg->seg_length)
+			currSeg = NULL;
 	}
 }
 
 // make segment the new current segment
 void State::newSegment(eecs_376_alpha::PathSegment segment)
 {
-	currSeg = segment;
+	// may also want to update x,y,and psi values
+	segDistDone = 0.0;
+	currSeg = &segment;
 }
 
 void State::stop()
@@ -106,7 +148,19 @@ double State::getSegDistDone()
 	return segDistDone;
 }
 
-eecs_376_alpha::PathSegment State::getSegment()
+eecs_376_alpha::PathSegment* State::getSegment()
 {
 	return currSeg;
 }
+/*
+void State::positionCallback(const nav_msgs::Odometry::ConstPtr& pose)
+{
+	geometry_msgs::Point position = pose->position;
+	geometry_msgs::Quaternion orientation = pose->orientation;
+
+	xAct = position.x;
+	yAct = position.y;
+
+	psiAct = tf::getYaw(orientation);
+}
+*/
--- src/state.h
+++ src/state.h
@@ -10,17 +10,19 @@
 
 #include <eecs_376_alpha/PathSegment.h>
 #include <tf/transform_datatypes.h>
+//#include <nav_msgs/Odometry.h>
+//#include <ros/ros.h>
 
 class State
 {
 public:
 	// constructors
-	State(double x0, double y0, double psi0);
-	State(double x0, double y0, double psi0, eecs_376_alpha::PathSegment segment);
+	State();
+	State(eecs_376_alpha::PathSegment segment);
 
 	//methods
 	void updateState(double vCmd, double dt);
-	void newSegment(eecs_376_alpha::PathSegment segment)
+	void newSegment(eecs_376_alpha::PathSegment segment);
 	void stop();
 
 	//getters
@@ -31,13 +33,17 @@ public:
 	double getVCmd(); // will return in m/s for arcs and straights and radians/s for spins
 	double getSegDistDone(); // will return in m for arcs and straights and radians for spins
 
-	eecs_376_alpha::PathSegment getSegment();
+	eecs_376_alpha::PathSegment* getSegment();
 
 private:
+	//ros::Subscriber sub;
 	double xPath,yPath,psiPath;
+	double xAct,yAct,psiAct;
 	double vCmd;
 	double segDistDone;
-	eecs_376_alpha::PathSegment currSeg;
+	eecs_376_alpha::PathSegment *currSeg;
+
+	//void poseCallback(const nav_msgs::Odometry::ConstPtr& pose);
 };
 
 #endif /* STATE_H_ */
