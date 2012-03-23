3b1ec3c86c057b9b20f05f9523aef248c8e41206
Devin Schwab <dts34@case.edu>
2012-03-23 12:59:31 -0400

    Changed velocity profiler package code to use msg_alpha messages instead of its own

--- velocity_profiler_alpha/src/look_ahead.cpp
+++ velocity_profiler_alpha/src/look_ahead.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <cwru_base/Pose.h>
 #include <sensor_msgs/LaserScan.h>
-#include <velocity_profiler_alpha/Obstacles.h>
+#include <msg_alpha/Obstacles.h>
 #include <iostream> 
 #include <math.h>
 #include <string>
@@ -43,7 +43,7 @@ void curPath(ros::Publisher &obsPub)
 {
   ros::Time time = ros::Time::now();
 
-  velocity_profiler_alpha::Obstacles obsData;  //create an instance of the obstacle msg
+  msg_alpha::Obstacles obsData;  //create an instance of the obstacle msg
   
   double closestObs = 90.0; // laser range is up to 80 so nothing should be worse than this
 
@@ -96,8 +96,8 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
     
-  ros::Subscriber laserSub = n.subscribe(cLaserTopic,1,laserCallback);
-  ros::Publisher obsPub = n.advertise<velocity_profiler_alpha::Obstacles>("obstacles",1);
+  ros::Subscriber laserSub = n.subscribe(cSimLaserTopic,1,laserCallback);
+  ros::Publisher obsPub = n.advertise<msg_alpha::Obstacles>("obstacles",1);
 
   while(!ros::Time::isValid()) {} //helps simulation initialization
 
