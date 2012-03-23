cf65e1665ffa053d7462577ff2667ba6d36983bf
Eddie Massey III <eem33@case.edu>
2012-03-23 10:56:01 -0400

    look_ahead.py correctly publishes messages, but bounding box alg is not working, Included rospy in the manifest, removed look_ahead.cpp from CMakeLists

--- velocity_profiler/src/look_ahead.py
+++ velocity_profiler/src/look_ahead.py
@@ -1,52 +1,70 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('laserScanner')
+import roslib; roslib.load_manifest('velocity_profiler')
 import math
 import rospy
 from sensor_msgs.msg import LaserScan
+from velocity_profiler.msg import Obstacles
+
+
+
+
 
 scanData=[]
-exists = False
-
-
 BOX_WIDTH = 0.5
-BOX_LENGTH = 1
-angleSwitch = math.atanh(BOX_WIDTH)
-distance = 0
+BOX_HEIGHT = 1.0
+angleSwitch = math.atanh(BOX_WIDTH) * 180/math.pi
+
+
 
 def laserCallback(data):
     for i in range(len(data.ranges)):
         scanData.append(data.ranges[i])
-
+        
 def straight():
+    obsPub = rospy.Publisher('obstacles', Obstacles)
+    obsData = Obstacles()
     closestObs = 90
+
     for x in range(len(scanData)):
         
-        if(x < 90 - angleSwitch or x > 90 + angleSwitch):
-            if(scanData[x] < BOX_WIDTH/math.cos(180-x) * math.pi/180):
+        if(x < 90-angleSwitch or x > 90+angleSwitch):
+
+            if(scanData[x] < BOX_WIDTH/math.cos((180.0-x) * math.pi/180)):
+
                 if(scanData[x] < closestObs):
+
                     closestObs = scanData[x]
 
-            elif(x > 90 - angleSwitch and x < 90 + angleSwitch):
-                if(scanData[x] < BOX_LENGTH/math.cos(x-90) * math.pi/180):
-                    if(scanData[x] < closestObs):
-                        closestObs = scanData[x]
+        elif(x > 90 - angleSwitch and x < 90 + angleSwitch):
+
+            if(scanData[x] < BOX_HEIGHT/math.cos((x-90) * math.pi/180)):
+
+                if(scanData[x] < closestObs):
+
+                    closestObs = scanData[x]
+
     if(closestObs < 90):
-        exists = true
-        distance = closestObs
+        obsData.exists = True
+        obsData.distance = closestObs
+    else:
+        obsData.exists = False
+        obsData.distance = 0.0
+        
+    obsPub.publish(obsData)
+
 
 def main():
+    
     rospy.init_node('n')
-    rospy.Subscriber("base_scan",LaserScan,laserCallback)
-#    obsPub = rospy.Publisher('obstacles',1)
-    r = rospy.Rate(10)
+    r = rospy.Rate(100)
+    rospy.Subscriber("base_scan",LaserScan,laserCallback)    
 
     while not rospy.is_shutdown():
+ 
         straight()
-        rospy.spin()
-        r.sleep()
-        
-
+    rospy.spin
 if __name__ == '__main__':
+
     main()
 
 
@@ -56,8 +74,8 @@ if __name__ == '__main__':
 
 
 
-                    
+    
 
 
 
-                        
+    
