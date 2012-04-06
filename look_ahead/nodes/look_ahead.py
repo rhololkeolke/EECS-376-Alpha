b6fa38e4c135ca0e6872c6570e7661d9af4e0abd
Eddie Massey III <eem33@case.edu>
2012-04-06 00:11:27 -0400

    changing algorithim for arc

--- look_ahead/nodes/look_ahead.py
+++ look_ahead/nodes/look_ahead.py
@@ -134,79 +134,9 @@ def arc(scanData):
     global initTanAngle
     global refPoint
 
-    radius = 0.5 #radius of the circle to be made around each lidar ping
-    theta = 0.0 #angle measure of circle
-    circles = [] #a list of each circle, each circle will be compared to the path circle.
-
-    #The starting point for the path segment of the robot should be (0,0) assuming the paths are passed in for scanning at the robots present location
-    xStartPt = 0.0 
-    yStartPt = 0.0
-
-    #The end point is the center of the path circle
-    xEndPt = refPoint.x
-    yEndPt = refPoint.y
-
-    #identify the mid point of the of the path segment
-    xMid = (xStartPt + xEndPt)/2.0
-    yMid = (yStartPt + yEndPt)/2.0
-
-    #Make a circle around the path segment
-    #Make a circle around each ping excluding the path segment
-    #If the distance between the circles is less than the sum of the radii an obstacle exists
-    #invariant: the iterator is always within bounds of the 181 pings of lidar data
-    for i in ranges(scanData):
-        
-        #if the lidar ping has reached the end point of the projected path segment
-        #create a circle around the path segment
-        if(i == xMid and scanData(i) == yMid): 
-            pathDiam = math.sqrt(math.pow(xEndPt - xStartPt),2.0 + math.pow(yEndPt + yStartPt))
-            pathRadius = pathDiam/2.0
 
 
-            xPathCent = i * pathRadius * math.sin(i)  #convert to polar coords.
-            yPathCent = scanData(i) * pathRadius * math.sin(i) #convert polar coords.
-            pathCircle = [xPathCent,yPathCent]  #path circle
-            
 
-        #create an circle around each lidar ping with the ping as the center
-        while(theta < 2.0 * math.pi):
-            
-            
-            #Do not turn lidar pings within the path segment circle or along the path segment into circles
-            #works under the premise that all points are within the radius of the circle and therefore
-            #dx = xCenter - x;
-            #dy = yCenter - y;
-            #dx*dx + dy+dy < radius * radius
-            #j is always within range of the lidar pings
-            for j in ranges(scanData):
-                
-                
-                dx = xPathCent - j 
-                dy = yPathCent- scanData(j)
-                
-                if(dx * dx + dy * dy < pathRadius * pathRadius):
-                    
-`                    pass
-
-                else:
-                    
-    
-                    theta += 0.1
-                    xCenter = i * radius * math.sin(theta) #create the x-coord for the center of the circle
-                    yCenter = scanData(i) * radius * math.cos(theta) # create the y-coord for the center of the circle
-                    circle = [xCenter,yCenter] #a list of circle information contains ifs centerpoint and location along lidar ping
-                    circles.append(circle)  # place each circle in a list of circles
-
-        #if the euclidean distance between a circle and the radii of the path segment circle is less than the sum of the radii then an obstacle exists else it does not
-        for c in ranges(circles):
-            if(math.sqrt(math.pow(circles[c][0] - pathCircle[0],2.0) + math.pow(circles[c][1] - pathCircle[1],2.0))  < radius + pathRadius):
-            
-                obsData.exists = True   #an obstacle exists
-            
-            else:
-                obsData.exists = False  #an obstacle does not exists
-                
-        obsPub.publish(obsData)  #publish the obstacle information
 
 def spin(scanData):
     global BOX_WIDTH #width of the bounded box for checkAhead
