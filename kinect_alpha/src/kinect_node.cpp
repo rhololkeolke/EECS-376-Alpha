01a59c1fbcb7dd621c1de1e93635af7c4973b466
Mark Schultz <mxs802@case.edu>
2012-04-09 21:51:31 -0400

    fix so window shows

--- kinect_alpha/src/kinect_node.cpp
+++ kinect_alpha/src/kinect_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
-#include <kinect_alpha/lib_blob.h>
+#include "lib_blob.h"
+#include <msg_alpha/BlobDistance.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
@@ -9,43 +10,22 @@
 
 #include <boost/format.hpp>
 
-/n/ OpenCV includes
+// OpenCV includes
 #include <image_transport/image_transport.h>
 #include <image_transport/subscriber_filter.h>
 #include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
 using std::string;
+namespace enc = sensor_msgs::image_encodings;
 
 // Global variables here
 ros::Publisher             cloud_pub_;
 image_transport::Publisher image_pub_;
 string window_name_;
-
-// A magical callback that combines an image, cam info, and point cloud
-void allCB(const sensor_msgs::ImageConstPtr& image_msg, 
-		   const sensor_msgs::PointCloud2::ConstPtr& cloud_msg,
-		   const sensor_msgs::CameraInfo::ConstPtr& cam_msg)
-{
-	// Convert the image from ROS format to OpenCV format
-	cv_bridge::CvImagePtr cv_ptr;
-	try	{
-		cv_ptr = cv_bridge::toCvCopy(image_msg);
-	}
-	catch (cv_bridge::Exception& e) {
-		ROS_ERROR_STREAM("cv_bridge exception: " << e.what());
-		return;
-	}
-	
-	ROS_INFO_STREAM(boost::format("Callback got an image in format %s, size %dx%d")
-		%cv_ptr->encoding %cv_ptr->image.size().width %cv_ptr->image.size().height );
-
-	// Show the image.  The window does not update without the cvWaitKey.
-	cv::imshow(window_name_.c_str(), cv_ptr->image);
-	cvWaitKey(5);
-}
 
 class KinectNode {
   public:
@@ -55,39 +35,45 @@ class KinectNode {
     ros::NodeHandle nh_;
     image_transport::ImageTransport it_;
     image_transport::Subscriber sub_;
-    image_transport::Publisher image_pub_;
+    //image_transport::Publisher image_pub_;
+    msg_alpha::BlobDistance blobDist;
+    ros::Publisher blobPub;
 };
 
 KinectNode::KinectNode():
   it_(nh_)
 {
-  sub_ = it_.subscribe("image", 1, &KinectNode::imageCallback, this);
-  image_pub_ = it_.advertise("out_image", 1);
+  sub_ = it_.subscribe("in_image", 1, &KinectNode::imageCallback, this);
+  //image_pub_ = it_.advertise("out_image", 1);
+  blobPub = nh_.advertise<msg_alpha::BlobDistance>("blob_dist",1);
 }
 
-void KinectNode::imageCallback(const sensor_msgs::ImageConstPtr& msg)
+void KinectNode::imageCallback(const sensor_msgs::ImageConstPtr& image_msg)
 {
-  sensor_msgs::CvBridge bridge;
-  cv::Mat image;
-  cv::Mat output;
-  try
-  {
-    image = cv::Mat(bridge.imgMsgToCv(msg, "bgr8"));
+	// Convert the image from ROS format to OpenCV format
+	cv_bridge::CvImagePtr cv_ptr;
+	try	{
+		cv_ptr = cv_bridge::toCvCopy(image_msg, enc::BGR8);
+	}
+	catch (cv_bridge::Exception& e) {
+		ROS_ERROR_STREAM("cv_bridge exception: " << e.what());
+		return;
+	}
+	
+	ROS_INFO_STREAM(boost::format("Callback got an image in format %s, size %dx%d")
+		%cv_ptr->encoding %cv_ptr->image.size().width %cv_ptr->image.size().height );
 
-  }
-  catch (sensor_msgs::CvBridgeException& e)
-  {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'. E was %s", msg->encoding.c_str(), e.what());
-  }
+  cv::Mat output, output1, output2;
   try {
-    normalizeColors(image, output);
-    blobfind(image, output);
-    findLines(image, output);
-    //cv::imshow("view", output);
+    //normalizeColors(cv_ptr->image, output);
+    blobfind(cv_ptr->image, output, blobDist.dist);
+    //findLines(cv_ptr->image, output);
+    cv::imshow("view", output);
     IplImage temp = output;
-    image_pub_.publish(bridge.cvToImgMsg(&temp, "bgr8"));
+    KinectNode::blobPub.publish(blobDist);
+    //image_pub_.publish(bridge.cvToImgMsg(&temp, "bgr8"));
   }
-  catch (sensor_msgs::CvBridgeException& e) {
+  catch (cv_bridge::Exception& e) {
 
     ROS_ERROR("Could not convert to 'bgr8'. Ex was %s", e.what());
   }
@@ -97,8 +83,8 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "kinect_alpha");
   KinectNode motion_tracker;
-  //cvNamedWindow("view"); //these cv* calls are need if you want to use cv::imshow anywhere in your program
-  //cvStartWindowThread();
+  cvNamedWindow("view"); //these cv* calls are need if you want to use cv::imshow anywhere in your program
+  cvStartWindowThread();
   ros::spin();
-  //cvDestroyWindow("view");
+  cvDestroyWindow("view");
 }
