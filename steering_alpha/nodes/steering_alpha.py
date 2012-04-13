329f502d16447bb8f3a4ad4386647ec83d125e0c
Mark Schultz <mxs802@case.edu>
2012-04-13 08:23:37 -0400

    work on main nl function

--- steering_alpha/nodes/steering_alpha.py
+++ steering_alpha/nodes/steering_alpha.py
@@ -3,6 +3,7 @@
 
 import roslib; roslib.load_manifest('steering_alpha')
 import rospy
+import numpy
 import tf
 
 from msg_alpha.msg._PathSegment import PathSegment as PathSegmentMsg
@@ -12,6 +13,7 @@ from geometry_msgs.msg._PoseStamped import PoseStamped as PoseStampedMsg
 from geometry_msgs.msg._Twist import Twist as TwistMsg
 from nav_msgs.msg._Odometry import Odometry as OdometryMsg
 
+RATE = 10
 obsExists = False
 obsDist = 0
 lastOdom = OdometryMsg()
@@ -20,6 +22,10 @@ tfl = tf.TransformListener()
 desVel = TwistMsg()
 nextSegExists = False
 nextSeg = PathSegmentMsg()
+segNumber = 0
+segComplete = False
+curSegExists = False
+progressMade = 0
 
 def obstaclesCallback(obsData):
     global obsExists, obsDist
@@ -34,7 +40,7 @@ def odomCallback(odomData):
     temp.header = lastOdom.header
     try:
         tfl.transformPose("map",temp,lastMapPose)
-except tf.TransformException:
+    except tf.TransformException:
         rospy.roserror("Transform Error")
 
 def velCallback(velData):
@@ -49,7 +55,7 @@ def pathSegCallback(pathData):
         nextSeg = pathData
 
 def segStatusCallback(statusData):
-    global segNumber, segComplete, curSegExists,progressMade
+    global segNumber, segComplete, curSegExists, progressMade
     segNumber = statusData.seg_number
     if !segComplete:
         segComplete = statusData.segComplete
@@ -60,6 +66,7 @@ if __name__ == '__main__':
     main()
 
 def main():
+    global RATE, lastMapPose, nextSeg
     rospy.init_node('steering_alpha')
     rospy.Publisher('cmd_vel',TwistMsg)
     rospy.Subscriber('obstacles',ObstaclesMsg,obstaclesCallback)
@@ -67,4 +74,22 @@ def main():
     rospy.Subscriber('des_vel',TwistMsg,velCallback)
     rospy.Subscriber('path_seg',PathSegmentMsg,pathSegCallback)
     rospy.Subscriber('seg_status',SegStatusMsg,segStatusCallback)
-    rospy.spin()
+    naptime = rospy.Rate(RATE)
+
+    while not rospy.is_shutdown():
+        while not tfl.canTransform("map", "odom", rospy.Time.now()):
+            pass # spin till we have some map data
+
+        #control params
+        #TODO: tune these
+        dThreshold = 1.0
+        kOmega = 30.0
+        omegaSat = 2.0
+
+        curSeg = nextSeg
+        xyStartCoords = array([1,0])
+        xyRobotCoords = array([lastMapPose.pose.position.x, 
+                               lastMapPose.pose.position.y])
+        psiPathSeg = tf.getYaw(curSeg.init_tan_angle)
+
+
