5d929206c34ec0abd15f63bccc4a2183a707dff3
Mark Schultz <mxs802@case.edu>
2012-04-24 19:05:05 -0400

    add launch and tweak params. add costmap publisher

--- costmap_alpha/src/costmap_alpha.cpp
+++ costmap_alpha/src/costmap_alpha.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 #include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d_publisher.h>
 
 int main(int argc, char** argv){
 
@@ -13,6 +14,7 @@ int main(int argc, char** argv){
 	//create a ROS wrapper for the costmap, passing it a name and a reference to a TransformListener
 	//which will configure itself based on parameters
 	costmap_2d::Costmap2DROS costmap_ros("costmap", tf);
+	costmap_2d::Costmap2DPublisher(n, 1, "map");
 
 	//we'll need a node handle
 	ros::NodeHandle n;
