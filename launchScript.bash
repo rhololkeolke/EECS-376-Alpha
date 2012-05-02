ab9b39c96bd19a4662964bbd557ec78508be26cd
Eddie Massey III <eem33@case.edu>
2012-05-02 18:21:40 -0400

    changed launch script to screens

--- launchScript.bash
+++ launchScript.bash
@@ -1,5 +1,15 @@
-cd ~/code/robotics/cwru-ros-pkg/cwru_semi_stable
-roslaunch cwru_bringup.launch 
-gnome-terminal --tab --tab
-cd cwru_nav
-roslaunch start_amcl_2ndfloor.launch 
+screen -d -m -r -S mysession
+screen -X -S mysession -p 1 stuff "~/code/robotics/cwru-ros-pkg/cwru_semi_stable; autotest"
+screen -X -S mysession -p 1 stuff "roslaunch cwru_bringup.launch; autotest"
+screen -X -S mysession -p 2 stuff "~/code/robotics/cwru-ros-pkg/cwru_semi_stable/cwru_nav; autotest"
+screen -X -S mysession -p 2 stuff "roslaunch start_amcl_2ndfloor.launch; autotest"
+
+ 
+
+
+
+
+
+
+
+
