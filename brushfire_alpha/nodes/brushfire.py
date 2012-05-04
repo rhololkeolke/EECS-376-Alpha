43061c7e209054c02023582d7714a8f081734cbd
Mark Schultz <mxs802@case.edu>
2012-05-03 22:54:34 -0400

    skeleton brushfire class

--- brushfire_alpha/nodes/brushfire.py
+++ brushfire_alpha/nodes/brushfire.py
@@ -1,1 +1,17 @@
 class BrushFire():
+    def __init__(self, ):
+        pass
+    def updateGlobalGrid():
+        pass
+    def extractLocal():
+        pass
+    def transformGridToMap(self, point):
+        pass
+    def transformMapToGrid(self, point):
+        pass
+    def brushfire():
+        pass
+    def computePath():
+        pass
+    def updateGoal():
+        pass
