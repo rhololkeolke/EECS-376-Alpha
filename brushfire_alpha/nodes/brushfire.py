967cd6f3adee55d50836ae2795db826adc7a6d8b
Mark Schultz <mxs802@case.edu>
2012-05-04 00:10:05 -0400

    add brushfire computation

--- brushfire_alpha/nodes/brushfire.py
+++ brushfire_alpha/nodes/brushfire.py
@@ -9,8 +9,81 @@ class BrushFire():
         pass
     def transformMapToGrid(self, point):
         pass
-    def brushfire():
-        pass
+    def getNeighbors(self, point):
+        '''
+        given a point in grid space find all of viable neighbors
+        return this as a list of tuples
+        '''
+        neighbors = list()
+
+        # up
+        x = point[0]
+        y = point[1] + 1
+        if(y < self.height and y >=0 and x < self.height and x >= 0):
+            neighbors.append((x,y))
+
+        # up right
+        x = point[0] + 1
+        y = point[1] + 1
+        if(y < self.height and y >=0 and x < self.height and x >= 0):
+            neighbors.append((x,y))
+
+        # right
+        x = point[0] + 1
+        y = point[1]
+        if(y < self.height and y >=0 and x < self.height and x >= 0):
+            neighbors.append((x,y))
+
+        # down right
+        x = point[0] + 1
+        y = point[1] - 1
+        if(y < self.height and y >=0 and x <self.height and x >= 0):
+            neighbors.append((x,y))
+
+        # down
+        x = point[0]
+        y = point[1] - 1
+        if(y < self.height and y >=0 and x < self.height and x >= 0):
+            neighbors.append((x,y))
+
+        # down left
+        x = point[0] - 1
+        y = point[1] - 1
+        if(y < self.height and y >=0 and x < self.height and x >= 0):
+            neighbors.append((x,y))
+
+        # left
+        x = point[0] - 1
+        y = point[1]
+        if(y < self.height and y >=0 and x < self.height and x >= 0):
+            neighbors.append((x,y))
+
+        # up left
+        x = point[0] - 1
+        y = point[1] + 1
+        if(y < self.height and y >=0 and x < self.height and x >= 0):
+            neighbors.append((x,y))
+
+        return neighbors
+
+    def brushfire(self, localMap):
+        '''
+        Given a square grid of obstacles runs brushfire and returns grid
+        '''
+        self.height = len(localMap)
+        seenZero = True
+        # if there are no zeros seen in a loop, we are done with brushfire
+        while seenZero:
+            seenZero = False
+            for r,row in enumerate(localMap):
+                for c,col in enumerate(localMap[r]):
+                    if localMap[r][c] == 0:
+                        seenZero = True
+                        neighbors = self.getNeighbors((r,c))
+                        for point in neighbors:
+                            localMap[point[0]][point[1]]+=1
+        return localMap
+
     def computePath():
         pass
     def updateGoal():
